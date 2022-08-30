42585d501f5fb0301a69b258717e6a146dafc3c5
terrence-celestine <terrencebutler89@gmail.com>
2022-08-30 09:39:11 -0700

    Saving WIP

--- src/UserList/UserList.test.tsx
+++ src/UserList/UserList.test.tsx
@@ -1,5 +1,14 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { UserList } from "./index";
+import { handlers } from "../mocks/handlers";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+  rest.get("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "hello there" }));
+  })
+);
 
 test("should have 1 user", () => {
   render(<UserList />);
@@ -13,4 +22,17 @@ test("should add user to list", () => {
   fireEvent.click(userButton);
   const addedUser = screen.getByText(/test user/i);
   expect(addedUser).toBeInTheDocument();
+});
+
+test("handles server error", async () => {
+  server.use(
+    rest.get("/greeting", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<UserList />);
+
+  expect(screen.getByRole("alert")).toHaveTextContent("Oops, failed to fetch!");
+  expect(screen.getByRole("button")).not.toBeDisabled();
 });
--- src/UserList/index.tsx
+++ src/UserList/index.tsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 export const UserList = () => {
   const [users, setUsers] = useState(["user 1"]);
+  const [myUsers, setMyUsers] = useState([]);
+  const isInitialRender = useRef(true);
+  useEffect(() => {
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+      return;
+    }
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    await fetch("https://randomuser.me/api?results=1")
+      .then((res: any) => res.json())
+      .then((data) => {
+        setMyUsers(data.results);
+      });
+  };
+
   const addUser = (e: any, username: string) => {
     e.preventDefault();
     setUsers((prevState) => [...prevState, username]);
   };
+
+  const renderUsers = (users: any) => {
+    return users.map((user: any) => <p key={user?.id}>{user?.name?.first}</p>);
+  };
+
   return (
     <>
       <ul>
@@ -14,11 +37,12 @@ export const UserList = () => {
       </ul>
       <form>
         <input
-          onClick={(e) => addUser(e,  "test user")}
+          onClick={(e) => addUser(e, "test user")}
           type="submit"
           value="Add User"
         />
       </form>
+      {myUsers?.length > 0 ? renderUsers(myUsers) : null}
     </>
   );
 };
